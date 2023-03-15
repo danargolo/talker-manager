fe149cfc292cbc82206bd5347570765a2eea23f9
Danilo Argolo <dan_argolo@hotmail.com>
2023-03-15 18:32:32 -0300

    [FEAT] Adiciona rota /talker

---
+++ src/fsUtils/fsReadFile.js
@@ -0,0 +1,17 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const readTalkers = async () => {
+  try {
+    const data = await fs.readFile(path.resolve(__dirname, '../talker.json'))
+    const response = JSON.parse(data);
+
+    return response;
+  } catch(e){
+    console.error(e.message)
+  }
+}
+
+module.exports = {
+  readTalkers,
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const { readTalkers } = require('./fsUtils/fsReadFile');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
+const ZERO = 0;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const talkers = await readTalkers();
+
+  talkers.lenght === ZERO
+    ? res.status(HTTP_OK_STATUS).json([])
+    : res.status(HTTP_OK_STATUS).json(talkers)
 });
 
 app.listen(PORT, () => {
